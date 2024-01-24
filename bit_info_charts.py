0462205094f743811e28056832b993313fdd9185
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-01-24 17:37:58 +0100

    script bit info charts

--- bit_info_charts.py
+++ bit_info_charts.py
@@ -1,14 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import requests
+import bs4
 import time
 
-base_url = "https://bitinfocharts/bitcoin/address/"
+
+base_url = "https://bitinfocharts.com/bitcoin/address/"
 headers = {
     "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     + " (KHTML, like Gecko) Chrome/61.0.3163.100Safari/537.36"
 }
 
-driver = webdriver.Chrome()
-driver.get(base_url + "1257To3UPv8yDWaz23jovVCTTEJNi5NpRc")
-time.sleep(5)
-driver.quit()
+r = requests.get(base_url + "15GWLfGNuqbSvP7EqYAvwhEs99vjgUXksC")
+soup = bs4.BeautifulSoup(r.text, "html.parser")
+table = soup.find("table", {"class": "table table-striped table-condensed"})
+if isinstance(table, bs4.Tag):
+    print(table.text)
+    small = table.find("small")
+    if isinstance(small, bs4.Tag):
+        print(small.get_text())
