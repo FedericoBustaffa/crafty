1e26fb44d073ba672f340539db25322e429e5936
FedericoBustaffa <kiko.bustaffa@gmail.com>
2024-01-25 11:17:54 +0100

    crafty

--- bit_info_charts.py
+++ bit_info_charts.py
@@ -1,18 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+import numpy as np
 
-
-base_url = "https://bitinfocharts.com/bitcoin/address/"
-headers = {
-    "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-    + " (KHTML, like Gecko) Chrome/61.0.3163.100Safari/537.36"
-}
-
-driver = webdriver.Chrome()
-driver.get(base_url + "15GWLfGNuqbSvP7EqYAvwhEs99vjgUXksC")
-smalls = driver.find_elements(By.TAG_NAME, "small")
-for s in smalls:
-    print(s.text)
-
-driver.quit()
+s = {0, 1, 2, 3, 4, 5}
+print(np.random.choice(list(s)))
